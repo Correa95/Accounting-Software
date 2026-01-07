09e72595a941c57835763160a359cbc39437f700
Correa95 <mathewcorrea19@gmail.com>
2026-01-06 20:16:42 -0500

    creating the class object

--- backend/src/main/java/com/project/backend/company/CompanyRepository.java
+++ backend/src/main/java/com/project/backend/company/CompanyRepository.java
@@ -11,7 +11,7 @@ public class CompanyRepository {
         return companies;
     }
 
-    // public Company getCompanyByIndex(int index){
+    public Company getCompanyByIndex(int index){
         return  companies.get(index);
     }
 
