8acf751fb6f21e8229f88094cb8080f141ce2267
Correa95 <mathewcorrea19@gmail.com>
2026-01-14 10:34:25 -0500

    adding spring data source

--- backend/src/main/java/com/project/backend/entity/Customer.java
+++ backend/src/main/java/com/project/backend/entity/Customer.java
@@ -7,20 +7,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="customer")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable=false)
     private String name;
+
+    @Column(nullable=false)
     private String email;
+
+    @Column(nullable=false)
     private String phone;
+
+    @Column(nullable=false)
     private String billingAddress;
+    
     private String shippingAddress;
     private String taxId;
     private int paymentTerm;
