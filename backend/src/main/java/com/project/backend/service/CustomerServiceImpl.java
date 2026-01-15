b353397346d9d5f86ebfd034a3d3df9d49ccfc91
Correa95 <mathewcorrea19@gmail.com>
2026-01-15 11:05:49 -0500

    creating the customer service

--- backend/src/main/java/com/project/backend/service/CustomerServiceImpl.java
+++ backend/src/main/java/com/project/backend/service/CustomerServiceImpl.java
@@ -1,5 +1,16 @@
 package com.project.backend.service;
 
-public class CustomerServiceImpl  {
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
+import com.project.backend.entity.Customer;
+import com.project.backend.repository.CustomerRepository;
+
+
+@AllArgsConstructor
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    public final CustomerRepository customerRepository;
     
+
 }
