e84138de9c08d4c9a53087c07c7b7bfdc7715b64
Correa95 <mathewcorrea19@gmail.com>
2026-01-15 11:25:58 -0500

    creating the customer service

--- backend/src/main/java/com/project/backend/web/CustomerController.java
+++ backend/src/main/java/com/project/backend/web/CustomerController.java
@@ -1,14 +1,32 @@
 package com.project.backend.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.backend.service.CompanyService;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-
-@RequestMapping
-@GetMapping("/customer")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    public final CompanyService companyService;
+
+    @GetMapping("path")
+    public ResponseEntity<List<Customer> getCustomers() {
+        return 
+    }
     
+
+
+
 
     
 
