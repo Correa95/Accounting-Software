b6ededead409a63a0b9472a02ca167d141145050
Correa95 <mathewcorrea19@gmail.com>
2026-01-15 11:21:01 -0500

    creating the customer service

--- backend/src/main/java/com/project/backend/service/CustomerService.java
+++ backend/src/main/java/com/project/backend/service/CustomerService.java
@@ -2,12 +2,16 @@ package com.project.backend.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.project.backend.entity.Customer;
 
-
+@Service
 public interface  CustomerService {
     List<Customer> getCustomers();
     Customer getCustomer(Long id);
     Customer savCustomer(Customer customer);
+    Customer updateCustomer(long id, Customer customer);
     void deleteCustomer(Long id);
+
 }
