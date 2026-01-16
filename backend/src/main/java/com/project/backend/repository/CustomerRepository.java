87c86eedfd2177bdacc600d140aa66a6a10bb118
Correa95 <mathewcorrea19@gmail.com>
2026-01-16 10:35:06 -0500

    modeling the customer relationship

--- backend/src/main/java/com/project/backend/repository/CustomerRepository.java
+++ backend/src/main/java/com/project/backend/repository/CustomerRepository.java
@@ -1,8 +1,14 @@
 package com.project.backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.project.backend.entity.Customer;
 
 public interface  CustomerRepository extends JpaRepository<Customer, Long>{
+    List<Customer> findByCompanyId(Long companyId);
+    Optional<Customer> 
     
 }
