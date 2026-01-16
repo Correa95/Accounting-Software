4a731c45ab899188e74d01ef05c145dc2085d23d
Correa95 <mathewcorrea19@gmail.com>
2026-01-16 10:26:23 -0500

    modeling the customer relationship

--- backend/src/main/java/com/project/backend/web/CustomerController.java
+++ backend/src/main/java/com/project/backend/web/CustomerController.java
@@ -1,22 +1,22 @@
 package com.project.backend.web;
 
 import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.backend.entity.Customer;
-
 import com.project.backend.service.CustomerService;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -63,22 +63,6 @@ public class CustomerController {
     customerService.deleteCustomer(customerId, companyId);
         return ResponseEntity.noContent().build();
     // return new ResponseEntity<>(customerService.deleteCustomer(customerId, companyId), HttpStatus.NO_CONTENT); // HTTP 204
-}
-        
+    }
+}  
 
-
-
-}
-    
-
-
-    
-    
-
-
-
-
-    
-
-    
-}
