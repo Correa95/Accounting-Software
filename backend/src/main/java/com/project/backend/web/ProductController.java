c95e10fdba0bd9fe3bb55ba6cd6c6f1e9a6cfb5e
Correa95 <mathewcorrea19@gmail.com>
2026-01-18 09:34:03 -0500

    validating product type

--- backend/src/main/java/com/project/backend/web/ProductController.java
+++ backend/src/main/java/com/project/backend/web/ProductController.java
@@ -2,9 +2,15 @@ package com.project.backend.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("companies/{companyId}/product")
 public class ProductController {
+
+    public 
 
     
 }
