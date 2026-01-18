b80ea3e97bc82f102347891cd465ede4ff8f5180
Correa95 <mathewcorrea19@gmail.com>
2026-01-18 09:36:59 -0500

    validating product type

--- backend/src/main/java/com/project/backend/web/ProductController.java
+++ backend/src/main/java/com/project/backend/web/ProductController.java
@@ -1,16 +1,25 @@
 package com.project.backend.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("companies/{companyId}/product")
 public class ProductController {
 
-    public 
+    @GetMapping()
+    public ResponseEntity List<Product> getProducts(@PathVariable long com) {
+        return new String();
+    }
+    
 
     
 }
