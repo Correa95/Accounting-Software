70b1f09656744692106eeed935779dd8879a0b17
Correa95 <mathewcorrea19@gmail.com>
2026-01-19 10:00:26 -0500

    creating the service offering controller routes

--- backend/src/main/java/com/project/backend/web/ServiceOfferingController.java
+++ backend/src/main/java/com/project/backend/web/ServiceOfferingController.java
@@ -8,13 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.backend.entity.ServiceOffering;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
-@RequestMapping("companies/{companyId}/serviceOffering")
+@RequestMapping("companies/{companyId}/serviceOfferings")
+
+    private final  ServiceOfferingService serviceOfferingService;
+
 public class ServiceOfferingController {
 
     @GetMapping("/{serviceOfferingId}")
     public ResponseEntity<ServiceOffering> getServiceOfferings(long companyId){
-        return new ResponseEntity<>(serviceOfferingRepository.getServiceOfferings(companyId), HttpStatus.OK);
+        return new ResponseEntity<>(serviceOfferingService.getServiceOfferings(companyId), HttpStatus.OK);
     }
     
 }
