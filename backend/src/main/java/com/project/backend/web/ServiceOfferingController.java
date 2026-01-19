274a93edf1ab00e3f7b758f235cd18799e778108
Correa95 <mathewcorrea19@gmail.com>
2026-01-19 10:35:41 -0500

    creating the service offering controller routes

--- backend/src/main/java/com/project/backend/web/ServiceOfferingController.java
+++ backend/src/main/java/com/project/backend/web/ServiceOfferingController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,6 +53,10 @@ public class ServiceOfferingController {
     public ResponseEntity<ServiceOffering> updateServiceOffering(@PathVariable long serviceOfferingId, @PathVariable long companyId, @RequestBody ServiceOffering serviceOffering) {
         return new ResponseEntity<>(serviceOfferingService.updateServiceOffering(serviceOfferingId, companyId, serviceOffering), HttpStatus.OK);
     }
+
+    // Deleting Service Offering
+    @DeleteMapping("/{serviceOfferingId}")
+    public void deleteServiceOffering(@PathVariable long serviceOfferingId, @PathVariable)
     
 
 
