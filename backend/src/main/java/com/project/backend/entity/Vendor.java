bd1adaae3fa76326ef23d45c373c15243b8fc577
Correa95 <mathewcorrea19@gmail.com>
2026-01-19 10:50:07 -0500

    creating the service offering controller routes

--- backend/src/main/java/com/project/backend/entity/Vendor.java
+++ backend/src/main/java/com/project/backend/entity/Vendor.java
@@ -1,17 +1,83 @@
 package com.project.backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vendor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    @Column(nullable = false)
+    private String vendorName;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String phone;
     private String address;
     private int vandorNumber;
     private String taxId;
     private String paymentTerms;
-    private boolean isActive;
+    private boolean active = true;
+
+    
+}
+package com.project.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Vendor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String vendorName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private int vendorNumber;
+
+    @Column(nullable = false)
+    private String taxId;
+
+    @Column(nullable = false)
+    private String paymentTerms;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "company_id", referencedColumnName = "id", nullable = false )
+    private Company company;
 
     
 }
