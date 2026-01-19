106199c62222272adb21573ebe36c2b1c4d8b569
Correa95 <mathewcorrea19@gmail.com>
2026-01-19 10:53:37 -0500

    creating the service offering controller routes

--- backend/src/main/java/com/project/backend/entity/Vendor.java
+++ backend/src/main/java/com/project/backend/entity/Vendor.java
@@ -1,42 +1,12 @@
 package com.project.backend.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Vendor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false)
-    private String vendorName;
-    @Column(nullable = false)
-    private String email;
-    @Column(nullable = false)
-    private String phone;
-    private String address;
-    private int vandorNumber;
-    private String taxId;
-    private String paymentTerms;
-    private boolean active = true;
-
-    
-}
-package com.project.backend.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
