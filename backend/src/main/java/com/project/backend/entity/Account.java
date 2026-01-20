abf8edb060150f0f7228edb3cd652ba6726a972f
Correa95 <mathewcorrea19@gmail.com>
2026-01-20 09:25:26 -0500

    debugging the vendors controller

--- backend/src/main/java/com/project/backend/entity/Account.java
+++ backend/src/main/java/com/project/backend/entity/Account.java
@@ -1,8 +1,18 @@
 package com.project.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 @Entity
+@Table(name = "accounts")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private 
     
 }
