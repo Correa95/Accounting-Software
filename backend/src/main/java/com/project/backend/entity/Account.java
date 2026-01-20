5837db96c4957e0dba0190ca3cc85370d1f50693
Correa95 <mathewcorrea19@gmail.com>
2026-01-20 09:41:09 -0500

    debugging the vendors controller

--- backend/src/main/java/com/project/backend/entity/Account.java
+++ backend/src/main/java/com/project/backend/entity/Account.java
@@ -3,12 +3,23 @@ package com.project.backend.entity;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "accounts")
 public class Account {
@@ -18,7 +29,11 @@ public class Account {
     private Long id; 
 
     private String accountName;
-    
+
     private BigDecimal balance;
+
+     @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
     
 }
