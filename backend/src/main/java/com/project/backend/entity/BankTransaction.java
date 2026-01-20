b776401e82dcf42a4d324c7f66c630fae78d600b
Correa95 <mathewcorrea19@gmail.com>
2026-01-20 09:30:44 -0500

    debugging the vendors controller

--- backend/src/main/java/com/project/backend/entity/BankTransaction.java
+++ backend/src/main/java/com/project/backend/entity/BankTransaction.java
@@ -1,8 +1,14 @@
 package com.project.backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "bank_transactions")
 public class BankTransaction {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 }
