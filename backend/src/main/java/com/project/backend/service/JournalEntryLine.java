86a5969fba850feda3e567589bfba9fc5a4bd368
Correa95 <mathewcorrea19@gmail.com>
2026-01-22 07:53:15 -0500

    working on the journal entry

--- backend/src/main/java/com/project/backend/service/JournalEntryLine.java
+++ backend/src/main/java/com/project/backend/service/JournalEntryLine.java
@@ -1,5 +1,16 @@
 package com.project.backend.service;
 
+import java.util.List;
+
+import com.project.backend.entity.JournalEntry;
+
 public interface  JournalEntryLine {
     List<JournalEntryLine> getLinesByJournalEntry(long journalEntryId, long companyId);
+
+    JournalEntryLine addJournalEntryLine(long journalEntryId, long companyId, JournalEntryLine journalEntryLine);
+
+    JournalEntryLine updateJournalEntryLine(long journalEntryId, long companyId, JournalEntryLine journalEntryLine);
+
+    void deleteLine(long )
+
 }
