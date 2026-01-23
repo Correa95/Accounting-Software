1d5335e07057d6aa1a48f5d8b2ea1ce5cc0024f1
Correa95 <mathewcorrea19@gmail.com>
2026-01-23 10:23:35 -0500

    completed the backend

--- backend/src/main/java/com/project/backend/web/BankAccountController.java
+++ backend/src/main/java/com/project/backend/web/BankAccountController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.project.backend.entity.Company;
 
 import com.project.backend.entity.BankAccount;
 import com.project.backend.service.BankAccountService;
