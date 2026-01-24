2fb73363c7c7dc8765a0824de946fcd2976d11a3
Correa95 <mathewcorrea19@gmail.com>
2026-01-24 11:28:25 -0500

    configuring h2 database

--- backend/src/main/java/com/project/backend/web/BankAccountController.java
+++ backend/src/main/java/com/project/backend/web/BankAccountController.java
@@ -3,12 +3,15 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.project.backend.entity.Company;
 
 import com.project.backend.entity.BankAccount;
 import com.project.backend.service.BankAccountService;
+import com.project.backend.service.CompmayService;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,12 +19,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.backend.service.CompanyService;
+
 @RestController
 @RequestMapping("/companies/{companyId}/bankAccounts")
 @RequiredArgsConstructor
 public class BankAccountController {
 
-    pri
+    private final CompanyService companyService;
     private final BankAccountService bankAccountService;
 
     @GetMapping  
