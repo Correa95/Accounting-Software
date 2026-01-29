16630dafb967cb9c81dcea308efe0fdc2458fc48
Correa95 <mathewcorrea19@gmail.com>
2026-01-29 11:33:50 -0500

    styling the navivation

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,19 @@
-import "./navBar.css";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { FaRegMessage } from "react-icons/fa6";
+import "./NavBar.css";
 function NavBar() {
   return (
     <div className="navBarContainer">
-      <h1>Tech</h1>
-
-      <div className="notification">
-        <div className="btn"></div>
+      <div className="logo">
+        <h1>Tech</h1>
+      </div>
+      <div className="notifications">
+        <button className="btnNot">
+          <IoNotificationsOutline size={30} />
+        </button>
+        <button className="btnMessage">
+          <FaRegMessage />
+        </button>
       </div>
     </div>
   );
