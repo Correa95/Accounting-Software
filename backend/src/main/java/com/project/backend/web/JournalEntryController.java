e79e56b1abbfd5b877564a3de0980dfac7f0c74d
Correa95 <mathewcorrea19@gmail.com>
2026-02-01 10:01:25 -0500

    fix the errors

--- backend/src/main/java/com/project/backend/web/JournalEntryController.java
+++ backend/src/main/java/com/project/backend/web/JournalEntryController.java
@@ -3,21 +3,18 @@ package com.project.backend.web;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-
-import com.project.backend.entity.JournalEntry;
-
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.project.backend.service.JournalEntryService;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.backend.entity.JournalEntry;
+import com.project.backend.service.JournalEntryService;
 
 
 @RestController
@@ -29,7 +26,7 @@ public class JournalEntryController {
         this.journalEntryService = journalEntryService;
     }
 
-    @GetMapping
+    @GetMapping("/journalEntry")
     public ResponseEntity<List<JournalEntry>> getAllJournalEntries(@PathVariable long companyId) {
         return new ResponseEntity<>(journalEntryService.getAllJournalEntries(companyId), HttpStatus.OK);
     }
