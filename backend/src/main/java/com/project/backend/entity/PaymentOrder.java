eebd7631ad750b354a255f3aa83c34e4168fba16
Correa95 <mathewcorrea19@gmail.com>
2026-02-02 11:06:23 -0500

    integrating stripe

--- backend/src/main/java/com/project/backend/entity/PaymentOrder.java
+++ backend/src/main/java/com/project/backend/entity/PaymentOrder.java
@@ -1,7 +1,6 @@
 package com.project.backend.entity;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.project.backend.enums.PaymentStatus;
@@ -10,12 +9,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
