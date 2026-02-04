8ad7ffc9df39c8d1d5cb104ae6d9cf3c4543b732
Correa95 <mathewcorrea19@gmail.com>
2026-02-04 09:44:11 -0500

    integrating stripe payment

--- backend/src/main/java/com/project/backend/service/StripeService.java
+++ backend/src/main/java/com/project/backend/service/StripeService.java
@@ -1,11 +1,23 @@
 package com.project.backend.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.project.backend.dto.PaymentRequest;
 import com.project.backend.dto.PaymentResponse;
+import com.project.backend.repository.PaymentOrderRepository;
 
-public interface StripeService {
+public class StripeService {
+    private final PaymentOrderRepository paymentOrderRepository;
+    @Transactional
+    public PaymentResponse createPaymentIntent(PaymentRequest paymentRequest){
+        try {
+            
+        } catch (Exception e) {
+            
+        }
+    }
 
-    PaymentResponse createPayment(PaymentRequest paymentRequest);
+    // PaymentResponse createPayment(PaymentRequest paymentRequest);
 
-    void handleWebhook(String payload, String sigHeader);
+    // void handleWebhook(String payload, String sigHeader);
 }
