70e20bcb11750aed1322027a18a468670ff9f074
Correa95 <mathewcorrea19@gmail.com>
2026-02-06 08:44:53 -0500

    removing stripe

--- backend/src/main/java/com/project/backend/entity/Account.java
+++ backend/src/main/java/com/project/backend/entity/Account.java
@@ -13,7 +13,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,12 +21,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(
-    name = "accounts",
-    indexes = {
-        @Index(name = "idx_account_number", columnList = "account_number")
-    }
-)
+@Table(name = "accounts")
 @Getter
 @Setter
 @NoArgsConstructor
