5070dca1a87c6230d2ab595e5145fe3e178218f2
Correa95 <mathewcorrea19@gmail.com>
2026-02-19 11:43:14 -0500

    configuring stripe apikey

--- backend/src/main/java/com/project/backend/config/StripeConfig.java
+++ backend/src/main/java/com/project/backend/config/StripeConfig.java
@@ -1,6 +1,8 @@
 package com.project.backend.config;
 
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value
 // import org.springframework.context.annotation.Bean;
 // import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
