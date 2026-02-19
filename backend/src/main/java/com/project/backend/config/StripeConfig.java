81936f876e9896a1aa2658131b24f889daf3581a
Correa95 <mathewcorrea19@gmail.com>
2026-02-19 11:38:12 -0500

    configuring stripe apikey

--- backend/src/main/java/com/project/backend/config/StripeConfig.java
+++ backend/src/main/java/com/project/backend/config/StripeConfig.java
@@ -1,28 +1,69 @@
 package com.project.backend.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.stripe.Stripe;
 
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
-
-
+import lombok.Value;
 @Configurable
-@Getter
 public class StripeConfig {
 
-    @Value("${stripe.api.key}")
-    private String apiKey;
-
-    @Value("${stripe.api.publishable-key}")
-    private String publishableKey;
-
-    @PostConstruct()
-    public void init(){
-        Stripe.apiKey = apiKey;
+    public StripeConfig(@Value("${stripe.secret.key}") String secretKey){
+        Stripe.apiKey = secretKey;
     }
+
+    // @Bean
+    // public WebMvcConfigurer corsConfigure(@Value("${app.frontend.url:http://localhost:3000}") String frontendUrl){
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry){
+    //             registry.addMapping(pathPattern"/""")
+    //             .allowedOrigins(frontendUrl)
+    //             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    //             .allowedHeader("*")
+    //             .allowedCredentials(true)
+    //             .maxAge(3600);
+    //         }
+    //     };
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @Value("${stripe.api.key}")
+    // private String apiKey;
+
+    // @Value("${stripe.api.publishable-key}")
+    // private String publishableKey;
+
+    // @PostConstruct()
+    // public void init(){
+    //     Stripe.apiKey = apiKey;
+    // }
 
     // @Value("${stripe.secret-key}")
     // private String stripeSecretKey;
@@ -36,4 +77,3 @@ public class StripeConfig {
     // void init() {
     //     Stripe.apiKey = stripeSecretKey;
     // }
-}
