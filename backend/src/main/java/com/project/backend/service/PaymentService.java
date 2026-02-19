83ec1f347f041a10cc4282f5e8ab3404a15cc2b2
Correa95 <mathewcorrea19@gmail.com>
2026-02-19 12:09:43 -0500

    configuring stripe apikey

--- backend/src/main/java/com/project/backend/service/PaymentService.java
+++ backend/src/main/java/com/project/backend/service/PaymentService.java
@@ -4,15 +4,43 @@ import java.util.List;
 import java.util.Optional;
 
 import com.project.backend.entity.Payment;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 
 public interface PaymentService {
 
-    List<Payment> getAllPayment(long invoiceId);
-    Payment getPayment(long invoice, long companyId);
+    public PatmentIntent createPayment(CreatePaymentRequest request) throws Exception{
+        PaymentIntentCreateParams params = PaymentIntentCreateParams.Builder()
+        .setAmount(request.getAmount())
+        .setCurrency(request.getCurrency())
+        .setDescription(request.getDescription())
+        .setAutomaticPaymentMethods(PaymentIntentCreateParams.AutomaticPaymentMethods
+            .builder()
+            .setEnabled(true)
+            .build())
+            .build();
+            return PaymentIntent.create(params);
+    }  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// List<Payment> getAllPayment(long invoiceId);
+//     Payment getPayment(long invoice, long companyId);
     
-    Payment makePayment(Payment paymentOrder);
+//     Payment makePayment(Payment paymentOrder);
 
     
-    Optional<Payment> findByStripePaymentIntentId(String stripePaymentIntentId);
-}
+//     Optional<Payment> findByStripePaymentIntentId(String stripePaymentIntentId);
 
